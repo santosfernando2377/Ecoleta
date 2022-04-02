bf422392a05f4b28e28c793f2bddbbc4192785e7
Fernando Soares <santosfernando2377@gmail.com>
2022-04-02 19:19:19 -0300

    Foi acrescentado o rect-router-dom no Web App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import Header from './components/header'
-import Main from './components/main'
-import Feature from './components/feature'
-import Footer from './components/footer'
+import Home from './pages/Home'
+import Page404 from './pages/404'
+
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className="bg-light">
-      <Header/>
-      <Main/>
-      <Feature/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='*' element={<Page404/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
