a00fc090230a6b05748e355002a14c6ba9a7cb63
Fernando Soares <santosfernando2377@gmail.com>
2022-04-16 14:21:52 -0300

    Foi adicionado a rota do dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from './pages/Home'
 import Page404 from './pages/404'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
@@ -13,6 +14,7 @@ export default function App() {
         <Route path='*' element={<Page404/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
   )
