bf5f58b53aa5bf2ac1122594f53e1c02f0730ccc
Fernando Soares <santosfernando2377@gmail.com>
2022-04-16 14:22:40 -0300

    Foi adicionado a tela de cadastro de usuário

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate, Link } from "react-router-dom";
 import logo from '../img/eco.png'
 
 export default function Register () {
+
+    const History = useNavigate()
 
     const Register = {
         nome: "",
         endereco: "",
         telefone: "",
-        eamil:""
+        email: "",
+        senha: ""
     }
 
     const [register, setRegister] = useState(Register)
@@ -21,8 +24,12 @@ export default function Register () {
 
     function onSumit (event) {
         event.preventDefault()
+        //History('/')
     }
 
+    function onValidateUser () {
+
+    }
     return (
         <>
         <div className='container d-flex flex-column justify-content-center align-items-center my-4'>
@@ -37,21 +44,25 @@ export default function Register () {
             <form onSubmit={onSumit}>
                 <div className="mb-3">
                     <label htmlFor="formGroupExampleInput" className="form-label">Nome Completo</label>
-                        <input type="text" className="form-control" name='nome' onChange={onChange} id="formGroupExampleInput" placeholder="Example input placeholder"/>
+                        <input type="text" className="form-control" name='nome' onChange={onChange} id="formGroupExampleInput" placeholder="Alberto Domingues"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="formGroupExampleInput2" className="form-label">Endereço</label>
-                        <input type="text" className="form-control" name='endereco' onChange={onChange} id="formGroupExampleInput2" placeholder="Another input placeholder"/>
+                        <input type="text" className="form-control" name='endereco' onChange={onChange} id="formGroupExampleInput2" placeholder="Rua Americanas 83 - Eco - Belo Horizonte - Minas Gerais"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="formGroupExampleInput3" className="form-label">Telefone</label>
-                        <input type="text" className="form-control" name='telefone' onChange={onChange} id="formGroupExampleInput3" placeholder="Another input placeholder"/>
+                        <input type="text" className="form-control" name='telefone' onChange={onChange} id="formGroupExampleInput3" placeholder="31912345678"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="formGroupExampleInput4" className="form-label">E-mail</label>
-                        <input type="text" className="form-control" name='email' onChange={onChange} id="formGroupExampleInput4" placeholder="Another input placeholder"/>
+                        <input type="text" className="form-control" name='email' onChange={onChange} id="formGroupExampleInput4" placeholder="eco@ecoleta.com.br"/>
                 </div>
-                <button className="w-100 btn btn-lg btn-success my-2" type="submit">Cadastrar</button>
+                <div className="mb-3">
+                    <label htmlFor="formGroupExampleInput4" className="form-label">Senha</label>
+                        <input type="text" className="form-control" name='senha' onChange={onChange} id="formGroupExampleInput4" placeholder="Super Coleta"/>
+                </div>
+                <button className="w-100 btn btn-lg btn-success my-2" type="submit" onClick={onValidateUser}>Cadastrar</button>
             </form>
         </div>
         </>
