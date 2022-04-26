58206ddd1d5235dfbc8bea9ca74914a46c284578
Fernando Soares <50937132+santosfernando2377@users.noreply.github.com>
2022-04-25 22:39:03 -0300

    Foi adicionado uma nova estilização para os botões

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function Card(props) {
     return (
-        <Link to={props.para} className="card w-50 my-2">
+        <Link to={props.para} className="card w-75 my-2 text-decoration-none text-muted shadow-sm">
             <div>
                 <div className="card-body">
                     <h5 className="card-title">{props.titulo}</h5>
