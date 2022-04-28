58a35304e5b95123d23fd6f6be9bd2bdf55c326a
Fernando Soares <50937132+santosfernando2377@users.noreply.github.com>
2022-04-28 08:47:40 -0300

    Foi criado o elemento Input para o formulário

---
+++ src/components/form/input.jsx
@@ -0,0 +1,8 @@
+export default function Input(props) {
+    return (
+        <div className="mb-3">
+            <label htmlFor={props.id} className="form-label">{props.label}</label>
+            <input type={props.type} className="form-control" id={props.id} defaultValue={props.value} placeholder={props.placeholder}/>
+        </div>
+    )
+}
