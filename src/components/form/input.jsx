660f32c4f9b528a2cd097ce7409e257a598d46d8
Fernando Soares <santosfernando2377@gmail.com>
2022-05-02 08:37:30 -0300

    Foi alterado o componente input

--- src/components/form/input.jsx
+++ src/components/form/input.jsx
@@ -2,7 +2,7 @@ export default function Input(props) {
     return (
         <div className="mb-3">
             <label htmlFor={props.id} className="form-label">{props.label}</label>
-            <input type={props.type} className="form-control" id={props.id} defaultValue={props.value} placeholder={props.placeholder} onChange={props.onChange}/>
+            <input type={props.type} className="form-control" name={props.id} id={props.id} defaultValue={props.defaultValue} placeholder={props.placeholder} onChange={props.onChange}/>
         </div>
     )
 }
